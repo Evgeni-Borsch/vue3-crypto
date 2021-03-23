6549782db30d466163b75b1c1bc5c9ccf5529224
unknown <borschewsky@gmail.com>
2021-03-23 15:51:09 +0300

    Add URL

--- src/App.vue
+++ src/App.vue
@@ -64,10 +64,20 @@
     </section>
     <template v-if="tickers.length > 0">
       <hr class="w-full border-t border-gray-600 my-4" />
+      <div>
+        <div>Фильтр: <input v-model="filter" /> </div>
+        <button v-if="page >1" @click="page = page-1" class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Назад
+        </button> 
+        <button v-if="hasNextPage" @click="page = page+1" class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Вперед
+        </button>
+      </div>
+      <hr class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-          v-for="t in tickers"
+          v-for="t in filteredTickers()"
           :key="t.name"
           @click="select(t)"
         >
@@ -155,29 +165,66 @@ export default {
       ticker: "",
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+      page: 1,
+      filter: '',
+      hasNextPage: true
     }
   },
+
+  created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+    if(windowData.filter) {
+      this.filter = windowData.filter
+    }
+    if(windowData.page) {
+      this.page = windowData.page
+    }
+
+    const tickersData = localStorage.getItem('cryptonomicon-list');
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name)
+      })
+    }    
+  },
+
   methods: {
+
+    filteredTickers() {
+      const start = (this.page - 1)*6;
+      const end = this.page*6;
+      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter))
+      this.hasNextPage = filteredTickers.length > end;
+      return filteredTickers.slice(start,end)
+    },
+    
+
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch (
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=73302d28d5bf9f8ff43620332515ace6d89a2a4a3d13bcf35fcb34ec81075908`
+        );
+        const data = await f.json();
+        this.tickers.find(t => t.name === tickerName).price = data.USD >1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      },5000);
+      this.ticker = '';
+    },
     add() {
       const currentTicker = {
         name: this.ticker, 
         price: '-'
       }
       this.tickers.push(currentTicker);
+      this.filter = '';
 
-      setInterval(async ()=> {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=73302d28d5bf9f8ff43620332515ace6d89a2a4a3d13bcf35fcb34ec81075908`
-        );
-        const data = await f.json();
-        this.tickers.find(t => t.name === currentTicker.name).price = data.USD >1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
-
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      },5000);
-      this.ticker = '';
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
     },
     select(ticker) {
       this.sel = ticker;
@@ -192,6 +239,25 @@ export default {
       const maxValue = Math.max(...this.graph);
       return this.graph.map(
         price => 5+ ((price - minValue)*95) / (maxValue - minValue)
+      )
+    }
+  },
+
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null, 
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      )
+    },
+    page() {
+      this.page = 1;
+      window.history.pushState(
+        null, 
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
       )
     }
   }
