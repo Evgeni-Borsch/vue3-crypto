6bd7d655eb270917c1b3cc5f3790f0c648e8fe8a
Evgeni <borschewsky@gmail.com>
2021-05-15 13:44:36 +0300

    divided to components

--- src/App.vue
+++ src/App.vue
@@ -7,61 +7,10 @@
     </svg>
   </div> -->
   <div class="container">
-    <section>
-      <div class="flex">
-        <div class="max-w-xs">
-          <label for="wallet" class="block text-sm font-medium text-gray-700"
-            >Тикер</label
-          >
-          <div class="mt-1 relative rounded-md shadow-md">
-            <input
-              type="text"
-              name="wallet"
-              id="wallet"
-              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-              placeholder="Например DOGE"
-              v-model="ticker"
-              @keydown.enter="add"
-            />
-          </div>
-          <div class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
-            <span @click="this.ticker = 'BTC'" class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              BTC
-            </span>
-            <span @click="this.ticker = 'DOGE'" class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              DOGE
-            </span>
-            <span @click="this.ticker = 'BNB'" class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              BNB
-            </span>
-            <span @click="this.ticker = 'ETH'" class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              ETH
-            </span>
-          </div>
-          <div v-if="this.tickers.indexOf(`${this.ticker}`)!==-1" class="text-sm text-red-600">Такой тикер уже добавлен</div>
-        </div>
-      </div>
-      <button
-        @click="add()"
-        type="button"
-        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      >
-        <!-- Heroicon name: solid/mail -->
-        <svg
-          class="-ml-0.5 mr-2 h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="#ffffff"
-        >
-          <path
-            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-          ></path>
-        </svg>
-        Добавить
-      </button>
-    </section>
+    <add-ticker 
+      @add-ticker="add"
+      :disabled="tooManyTickersAdded"
+    />
     <template v-if="tickers.length > 0">
       <hr class="w-full border-t border-gray-600 my-4" />
       <div>
@@ -111,63 +60,31 @@
       </dl>
       <hr class="w-full border-t border-gray-600 my-4" />
     </template>
-    <section v-if="selectedTicker" class="relative">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{ selectedTicker.name }} - USD
-      </h3>
-      <div class="flex items-end border-gray-600 border-b border-l h-64" ref="graph">
-        <div
-          v-for="(bar, idx) in normalizedGraph"
-          :key="idx"
-          :style="{height: `${bar}%`}"
-          class="bg-purple-800 border w-10"
-        ></div>
-      </div>
-      <button
-        @click="selectedTicker = null"
-        type="button"
-        class="absolute top-0 right-0"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.com/svgjs"
-          version="1.1"
-          width="30"
-          height="30"
-          x="0"
-          y="0"
-          viewBox="0 0 511.76 511.76"
-          style="enable-background:new 0 0 512 512"
-          xml:space="preserve"
-        >
-          <g>
-            <path
-              d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-              fill="#718096"
-              data-original="#000000"
-            ></path>
-          </g>
-        </svg>
-      </button>
-    </section>
+      <add-graph
+        v-if="selectedTicker"
+        @add-graph="select"
+        :selected-ticker="selectedTicker"
+      />
   </div>
 </div>
 </template>
 
 <script>
 import {subscribeToTicker} from './api';
+import AddTicker from './components/AddTicker.vue'
 
 export default {
   name: 'App',
   data() {
     return{
-      ticker: "",
       tickers: [],
       selectedTicker: null,
       graph: [],
       page: 1,
       filter: ''    }
+  },
+  components:{
+    AddTicker
   },
   mounted(){
     window.addEventListener('resize', this.calculateMaxGraphElements)
@@ -198,6 +115,9 @@ export default {
     setInterval( this.updateTickers,5000);
   },
   computed: {
+    tooManyTickersAdded(){
+      return this.tickers.length > 6
+    },
     calculateMaxGraphElements(){
       if(!this.$refs.graph){
         return
@@ -268,10 +188,11 @@ export default {
           }
           t.price = price;
         });
+        // console.log(this.tickers);
     },
-    add() {
+    add(ticker) {
       const currentTicker = {
-        name: this.ticker, 
+        name: ticker, 
         price: '-'
       }
       this.tickers = [...this.tickers,currentTicker];
@@ -282,6 +203,7 @@ export default {
     },
     select(ticker) {
       this.selectedTicker = ticker
+      // console.log(this.selectedTicker);
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(t => t != tickerToRemove);
